ff49e164cdaf896df670fe4cf93e0bf7e95e87e9
codercola <codercola034@gmail.com>
2024-08-10 11:49:49 +0800

    update custom keymapping

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -1,9 +1,18 @@
 vim.opt.wrap = true
 
+-- save all files at once
+vim.keymap.set("n", "<leader>w", "<cmd>wa<cr>", { silent = true })
+
+-- open definition in new split
 vim.keymap.set("n", "<leader>a", "<cmd>lua vim.lsp.buf.definition()<cr>", { silent = true })
 vim.keymap.set("n", "<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", { silent = true })
 vim.keymap.set("n", "<leader>s", "<cmd>belowright split | lua vim.lsp.buf.definition()<cr>", { silent = true })
-vim.keymap.set("n", "<leader>w", "<cmd>wa<cr>", { silent = true })
+
+-- telescope
+-- search symbol
+vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", { silent = true })
+vim.keymap.set("n", "<C-f>", "<cmd>Telescope live_grep<cr>", { silent = true })
+vim.keymap.set("n", "<C-s>", "<cmd>Telescope lsp_document_symbols<cr>", { silent = true })
 
 return {
   {
@@ -14,62 +23,6 @@ return {
     config = function()
       vim.cmd("colorscheme solarized-osaka")
     end,
-  },
-  {
-    'nanozuki/tabby.nvim',
-    event = 'VimEnter',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      local theme = {
-        fill = 'TabLineFill',
-        -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
-        head = 'TabLine',
-        current_tab = 'TabLineSel',
-        tab = 'TabLine',
-        win = 'TabLine',
-        tail = 'TabLine',
-      }
-      require('tabby.tabline').set(function(line)
-        return {
-          {
-            { '  ', hl = theme.head },
-            line.sep('', theme.head, theme.fill),
-          },
-          line.tabs().foreach(function(tab)
-            local hl = tab.is_current() and theme.current_tab or theme.tab
-            return {
-              line.sep('', hl, theme.fill),
-              tab.is_current() and '' or '󰆣',
-              tab.number(),
-              tab.name(),
-              tab.close_btn(''),
-              line.sep('', hl, theme.fill),
-              hl = hl,
-              margin = ' ',
-            }
-          end),
-          line.spacer(),
-          line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-            return {
-              line.sep('', theme.win, theme.fill),
-              win.is_current() and '' or '',
-              win.buf_name(),
-              line.sep('', theme.win, theme.fill),
-              hl = theme.win,
-              margin = ' ',
-            }
-          end),
-          {
-            line.sep('', theme.tail, theme.fill),
-            { '  ', hl = theme.tail },
-          },
-          hl = theme.fill,
-        }
-      end)
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
   },
   {
     "nvim-telescope/telescope.nvim",
@@ -93,9 +46,9 @@ return {
     event = "VeryLazy",
     config = function()
       vim.keymap.set("n", "<leader>dn", "<cmd>TestNearest -v<cr>", { silent = true })
+      vim.keymap.set("n", "<leader>dl", "<cmd>TestLast<cr>", { silent = true })
       -- vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>", { silent = true })
       -- vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<cr>", { silent = true })
-      -- vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>", { silent = true })
       -- vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<cr>", { silent = true })
     end,
 
