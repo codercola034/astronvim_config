aba043c00f3ec39a13bf241409c3105c6999dc47
codercola <codercola034@gmail.com>
2024-08-10 11:49:37 +0800

    remvoe ensure install list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,27 +5,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-      "json",
-      "yaml",
-      "toml",
-      "html",
-      "css",
-      "javascript",
-      "typescript",
-      "tsx",
-      "python",
-      "rust",
-      "c",
-      "cpp",
-      "java",
-      "go",
-      "bash",
-      "regex",
-      "dockerfile",
-    })
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { })
   end,
 }
