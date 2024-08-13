159fed4971484ae325cc4ea7e657efdb72d27d9f
jacob.lee <jacob.lee@winpromise.com>
2024-08-13 21:29:33 +0800

    add auto format file types

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -21,7 +21,7 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          "go",
+          "go", "js", "jsx", "ts", "tsx", "css"
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
