99e2c2a212687195924a4f836354a6b723439387
codercola <codercola034@gmail.com>
2025-01-08 10:31:38 +0800

    refactor: add supermaven

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -106,16 +106,4 @@ return {
       end
     end,
   },
-  -- {
-  --   "github/copilot.vim",
-  --   event = "VeryLazy",
-  --   autoStart = true,
-  --   config = function()
-  --     vim.g.copilot_no_tab_map = true
-  --     vim.g.copilot_assume_mapped = true
-  --     vim.g.copilot_tab_fallback = ""
-  --     vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  --     vim.api.nvim_set_keymap("i", "<c-e>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  --   end,
-  -- },
 }
