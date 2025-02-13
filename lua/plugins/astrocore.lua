926ddeaa08104220a9f1fbcd3680b12101e994ef
codercola <codercola034@gmail.com>
2025-02-13 10:58:44 +0800

    feat(mapping): add auto center cursor after go to defination

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -4,82 +4,120 @@
 --       as this provides autocomplete and documentation while editing
 
 ---@type LazySpec
-return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-        wrap = true, -- sets vim.opt.wrap
+return  {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      -- Configure core features of AstroNvim
+      features = {
+        large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+        autopairs = true, -- enable autopairs at start
+        cmp = true, -- enable completion at start
+        diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+        highlighturl = true, -- highlight URLs at start
+        notifications = true, -- enable notifications at start
       },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
+      -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+      diagnostics = {
+        virtual_text = true,
+        underline = true,
       },
-    },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      n = {
-        -- second key is the lefthand side of the map
+      -- vim options can be configured here
+      options = {
+        opt = { -- vim.opt.<key>
+          relativenumber = true, -- sets vim.opt.relativenumber
+          number = true, -- sets vim.opt.number
+          spell = true, -- sets vim.opt.spell
+          signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+          wrap = true, -- sets vim.opt.wrap
+        },
+        g = { -- vim.g.<key>
+          -- configure global vim variables (vim.g)
+          -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+          -- This can be found in the `lua/lazy_setup.lua` file
+        },
+      },
+      -- Mappings can be configured through AstroCore as well.
+      -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- second key is the lefthand side of the map
 
-        -- navigate buffer tabs with `H` and `L`
-        L = {
-          function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-          desc = "Next buffer",
-        },
-        H = {
-          function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-          desc = "Previous buffer",
-        },
-        -- open definition in new split
-        ["<leader>a"] = "<cmd>lua vim.lsp.buf.definition()<cr>",
-        ["<leader>v"] = "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>",
-        ["<leader>s"] = "<cmd>belowright split | lua vim.lsp.buf.definition()<cr>",
-        -- telescope
-        ["<C-p>"] = "<cmd>Telescope find_files<cr>",
-        ["<C-f>"] = "<cmd>Telescope live_grep<cr>",
+          -- navigate buffer tabs with `H` and `L`
+          L = {
+            function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+            desc = "Next buffer",
+          },
+          H = {
+            function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+            desc = "Previous buffer",
+          },
+          -- open definition in new split
+          ["<leader>v"] = { "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", desc = "Vsplit Definition" },
+          ["<leader>s"] = { "<cmd>belowright split | lua vim.lsp.buf.definition()<cr>", desc = "Split Definiion" },
+          -- telescope
+          ["<C-p>"] = "<cmd>Telescope find_files<cr>",
+          ["<C-f>"] = "<cmd>Telescope live_grep<cr>",
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bD"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Pick to close",
+          -- mappings seen under group name "Buffer"
+          ["<Leader>bD"] = {
+            function()
+              require("astroui.status.heirline").buffer_picker(
+                function(bufnr) require("astrocore.buffer").close(bufnr) end
+              )
+            end,
+            desc = "Pick to close",
+          },
+          -- tables with just a `desc` key will be registered with which-key if it's installed
+          -- this is useful for naming menus
+          ["<Leader>b"] = { desc = "Buffers" },
+          -- quick save all
+          ["<leader>a"] = { ":wa!<cr>", desc = "Save All" },
         },
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        ["<Leader>b"] = { desc = "Buffers" },
-        -- quick save
-        ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-      },
-      t = {
-        -- setting a mapping to false will disable it
-        -- ["<esc>"] = false,
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
       },
     },
   },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          -- this mapping will only be set in buffers with an LSP attached
+          K = {
+            function()
+              vim.lsp.buf.hover()
+            end,
+            desc = "Hover symbol details",
+          },
+          -- condition for only server with declaration capabilities
+          gD = {
+            function()
+              vim.lsp.buf.declaration()
+              vim.defer_fn(function()
+                vim.cmd('normal! zz')
+              end, 1)
+            end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
+          gd = {
+            function()
+              vim.lsp.buf.definition()
+              vim.defer_fn(function()
+                vim.cmd('normal! zz')
+              end, 1)
+            end,
+            desc = "Go to Definition",
+          },
+        },
+      },
+    },
+  }
 }
