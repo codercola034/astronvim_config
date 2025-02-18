e3f12d89bbc78669b5c49fa8768351eee8f4cdde
codercola <codercola034@gmail.com>
2025-02-18 10:35:54 +0800

    change neotree position to left

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -17,7 +17,7 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       window = {
-        position = "right",
+        position = "left",
         width = 100,
       },
       filesystem= {
